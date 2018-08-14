370e042978343d26e5ad3de5a240c822de407acd
Ryan Murphy <murjax@gmail.com>
2018-08-13 22:53:59 -0400

    Solve

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-export default {
-  //implement me
+export default class CleverArray{
+  constructor(...items){
+    this.items = items;
+  }
+
+  get sum(){
+    if(this.items.length){
+      return this.items.reduce((accumulator, value) => accumulator + value, 0);
+    }
+  }
+
+  push(...moreItems){
+    this.items = this.items.concat(moreItems);
+  }
+
+  get average(){
+    return this.sum / this.items.length;
+  }
 }
